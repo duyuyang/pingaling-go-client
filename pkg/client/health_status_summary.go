91c274436272f36672da36c65f44fb42aeadb2e9
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-09-30 15:19:09 +1000

    Extract api client to new file

--- pkg/client/health_status_summary.go
+++ pkg/client/health_status_summary.go
@@ -1,12 +1,5 @@
 package client
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
-)
-
 type HealthStatus struct {
 	Url     string `json:"url"`
 	Updated string `json:"updated"`
@@ -20,30 +13,10 @@ type HealthStatuses struct {
 }
 
 func HealthStatusSummary() []HealthStatus {
-	url := "http://localhost:4000/api/health/summary"
-
-	pingalingClient := http.Client{}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res, getErr := pingalingClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
+	path := "/health/summary"
 	var statuses HealthStatuses
-	jsonErr := json.Unmarshal(body, &statuses)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
+
+	ApiGet(path, &statuses)
 
 	return statuses.Data
 }
