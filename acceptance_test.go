44542ed283b9ed8cb479b875cf38325b1808cb75
Du Yuyang <du.r.yuyang@gmail.com>
2018-10-21 12:25:09 +1100

    add acceptance tests

---
+++ acceptance_test.go
@@ -0,0 +1,26 @@
+// +build acceptance
+
+package main_test
+
+import (
+	"bytes"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCmdVersion(t *testing.T) {
+
+	cmd := exec.Command("./pingaling", "version")
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "0.5.0\n", out.String())
+
+}
